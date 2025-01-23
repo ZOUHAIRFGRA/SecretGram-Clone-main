f4fa96383dc6286ce6fb2a3e0879324b28a75b53
Zouhair Fouiguira <zouhairfgra@gmail.com>
2025-01-23 21:37:15 +0100

    uodate app url

--- client/src/hooks/useFetch.jsx
+++ client/src/hooks/useFetch.jsx
@@ -21,7 +21,7 @@ const useFetch = ({ method, url }, successFn, errorFn) => {
 
     try {
       setRequestState("loading");
-      const response = await fetch(`/api${url}`, fetchOptions);
+      const response = await fetch(`https://greyline.onrender.com/api${url}`, fetchOptions);
       let data;
       if (methodUpper !== "DELETE") {
         data = await response.json();
